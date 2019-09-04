afce489590c4f4fbb0c29447321460afb5416c7c
Jay Spendlove <jayclark24@gmail.com>
2019-09-03 21:24:52 -0700

    First commit

---
+++ calculator/trial.py
@@ -0,0 +1,24 @@
+
+#First test to test python code in this directory
+
+
+def square(x):
+    """Finds the square of the input.
+
+    Args:
+        x (float): The number to be squared.
+
+    Returns:
+        x2 (float): The squared number.
+    """
+
+    return x**2
+
+
+
+print('Input Number');
+num = input();
+print(square(num));
+
+
+
