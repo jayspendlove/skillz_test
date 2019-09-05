0bbb83342b645c983c806e7792cc81e748f719cc
Jay Spendlove <jayclark24@gmail.com>
2019-09-05 08:08:12 -0700

    Commit basic skeleton code for add, subtract, and multiply functions

---
+++ calculator/calculator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+ #Basic four function calculator program with user input
+
+
+def add (num1, num2):
+	sum = num1 + num2
+
+	return sum
+
+
+def subtract(num1, num2):
+
+	return num1 - num2
+
+
+def multiply(num1, num2):
+	
+	return num1*num2
+
+
+def divide (num1, num2):
+	
+	if(num2 == 0);
+		print('Error');
+	
+
+
+
+
+def ask_operations()
