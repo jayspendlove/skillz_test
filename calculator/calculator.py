b71e8843ce43e8ea8b07176b69c4f2eec2ed03da
Jay Spendlove <jayclark24@gmail.com>
2019-09-08 16:15:25 -0700

    made all indentaion uniform

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -8,137 +8,118 @@
 
 class Calculator (object):
 
-    gOperation=0
-    gNum1=0
-    gNum2=0
-    
-    
-    
-    def main(self):
-        '''Main function to put all the pieces together, and execute inside a while loop'''
+	def main(self):
+		'''Main function to put all the pieces together, and execute inside a while loop'''
+		calculate = True;
+		print (calculate)
+
         
+	        calculate = True
+        	print(calculate)
         
-        #print(add(1,2))
+        	print("Hi! Welcome to Jay's Calculator!") 
         
-        calculate = True
-        print(calculate)
-        
-        print("Hi! Welcome to Jay's Calculator!")
-                   
-        
-        while (calculate):
+        	while (calculate):
+
+	    		
+			
+			
+			
+			self.__init__()
             
-            self.gOperation =0
-            self.gNum= 0
-            self.gNum2 = 0
             
-            print("Continue calculating? [y / n]")
+            	if (input("Continue calculating? [y / n]") == 'n'):
+                	calculate = False
+            	else:
+                	self.initialize_globals()
             
-            if (input() == 'n'):
-                calculate = False
-            else:
-                self.initialize_globals()
-            
-                self.eval_operation()
+                	self.eval_operation()
                 print("end of loop")
-        return
-    
+        	return
+	
 
-    '''______________________________________________________________'''
-    def add (self,num1, num2):	
-        '''function to compute the sum of two numbers'''
+	def __init__(self):
+                '''Initializes global variables to 0'''
 
-        return num1 + num2
-
-    '''________________________________________________________________'''
-    def subtract(self, num1, num2):
-        '''function to subtract 2 numbers'''
-
-        return num1 - num2
-
-    '''___________________________________________________________________'''
-    def multiply(self, num1, num2):
-        '''function to multiply 2 numbers'''
-
-        return num1*num2
-
-    '''______________________________________________________________'''
-    def divide (self, num1, num2):
-        '''function to divide 2 numbers'''
-
-        return num1/num2
+                self.gOperation=0
+                self.gNum1=0
+                self.gNum2=0    
+    		return
 
 
-    '''_______________________________________________________________'''
-    def ask_operation(self):
-        '''function to recieve user input for specific operation'''
+	def add (self,num1, num2):	
+        	'''function to compute the sum of two numbers'''
+
+        	return num1 + num2
+
+	def subtract(self, num1, num2):
+        	'''function to subtract 2 numbers'''
+
+        	return num1 - num2
+
+	def multiply(self, num1, num2):
+        	'''function to multiply 2 numbers'''
+
+        	return num1*num2
+
+	def divide (self, num1, num2):
+        	'''function to divide 2 numbers'''
+
+        	return num1/num2
+
+	def ask_operation(self):
+        	'''function to recieve user input for specific operation'''
 
 
-        operation = input('Please select operation - \
+        	operation = input('Please select operation - \
                             1. Add \
                             2. Subtract \
                             3. Multply \
                             4. Divide \
                                       ')
 
-        return operation
-    
+        	return operation
 
-    '''__________________________________________________________________'''
-    def ask_num(self):
-        '''function to recieve user input for specific numbers'''
-        num = input('Enter number:')
-        return num
+	def ask_num(self):
+        	'''function to recieve user input for specific numbers'''
+        	num = input('Enter number:')
+        	return num
 
-    
-    '''_____________________________________________________________________'''
-    def initialize_globals(self):
-        '''Function to initialize global variables with operations ask_operation() and ask_num()'''
-        self.gOperation = self.ask_operation()
-        self.gNum1 = self.ask_num()
-        self.gNum2 = self.ask_num()
-        
-        print("gOperation is ", gOperation, " and gNum1 and gNum2 are ", gNum1, " & ", gNum2)
+	def initialize_globals(self):
+        	'''Function to initialize global variables with operations ask_operation() and ask_num()'''
+        	self.gOperation = self.ask_operation()
+        	self.gNum1 = self.ask_num()
+        	self.gNum2 = self.ask_num()
 
-        return
+        	print("gOperation is ", gOperation, " and gNum1 and gNum2 are ", gNum1, " & ", gNum2)
 
-    '''_____________________________________________________________________________'''
-    def eval_operation(self):
-        '''Function to do the computations with the inputted operation and numbers,
-            and to test for 2 specific errors of 1) bad input for operation, and 
-            2) divide by 0 error'''
+        	return
 
-        if(gOperation == 1):
-            print(gNum1, " + ", gNum2, " = ",
-                    self.add(gNum1, gNum2))
-        elif (gOperation == 2):
-            print(gNum1, " - ", gNum2, " = ",
-                    self.subtract(gNum1, gNum2))
-        elif (gOperation == 3):
-            print(gNum1, " x ", gNum2, " = ",
-                    self.multiply (gNum1, gNum2))
-        elif (gOperation == 4):
-            if(gNum2 == 0):
-                print('Divide by 0 error')
-            else:
-                print(gNum1, " / ", gNum2, " = ",
-                    self.divide(gNum1, gNum2))
-        else:
-            self.gOperation = 0
-            print ('error')
-        
-        return
-    
-    
-    '''_________________________________________________________________________________________'''
-    
+	def eval_operation(self):
+        	'''Function to do the computations with the inputted operation and numbers,
+            	and to test for 2 specific errors of 1) bad input for operation, and 
+            	2) divide by 0 error'''
 
-    
-c1 = Calculator()
-c1.main()
-            
-        
-        
+        	if(gOperation == 1):
+            		print(gNum1, " + ", gNum2, " = ",
+                    	self.add(gNum1, gNum2))
+        	elif (gOperation == 2):
+            		print(gNum1, " - ", gNum2, " = ",
+                    	self.subtract(gNum1, gNum2))
+        	elif (gOperation == 3):
+            		print(gNum1, " x ", gNum2, " = ",
+                    	self.multiply (gNum1, gNum2))
+        	elif (gOperation == 4):
+            		if(gNum2 == 0):
+                		print('Divide by 0 error')
+            		else:
+                		print(gNum1, " / ", gNum2, " = ",
+                    		self.divide(gNum1, gNum2))
+        	else:
+            		self.gOperation = 0
+            		print ('error')
 
+        	return
 
-
+#c1 = Calculator()
+#c1.main()
