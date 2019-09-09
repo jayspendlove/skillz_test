aa84b385279a7411880cb76069542ec682254418
Jay Spendlove <jayclark24@gmail.com>
2019-09-08 17:34:30 -0700

    Calculator worksgit statusgit status

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -7,40 +7,37 @@
 	'''
 
 class Calculator (object):
-
+	
 	def main(self):
 		'''Main function to put all the pieces together, and execute inside a while loop'''
 		calculate = True;
 		print (calculate)
 
         
-	        calculate = True
-        	print(calculate)
-        
         	print("Hi! Welcome to Jay's Calculator!") 
         
         	while (calculate):
 
-	    		
-			
-			
-			
 			self.__init__()
+			res = raw_input("COntinue?")
+			print("gobble-dee-gook")
+            		if (res  == 'n'):
+                		calculate = False
+            		else:
+                		self.initialize_globals()
             
-            
-            	if (input("Continue calculating? [y / n]") == 'n'):
-                	calculate = False
-            	else:
-                	self.initialize_globals()
-            
-                	self.eval_operation()
-                print("end of loop")
+                		self.eval_operation()
+                	print("end of loop")
         	return
 	
 
 	def __init__(self):
                 '''Initializes global variables to 0'''
 
+
+		global gOperation
+        	global gNum1
+        	global gNum2
                 self.gOperation=0
                 self.gNum1=0
                 self.gNum2=0    
@@ -91,7 +88,7 @@ class Calculator (object):
         	self.gNum1 = self.ask_num()
         	self.gNum2 = self.ask_num()
 
-        	print("gOperation is ", gOperation, " and gNum1 and gNum2 are ", gNum1, " & ", gNum2)
+        	###print("gOperation is ", gOperation, " and gNum1 and gNum2 are ", gNum1, " & ", gNum2)
 
         	return
 
@@ -100,26 +97,24 @@ class Calculator (object):
             	and to test for 2 specific errors of 1) bad input for operation, and 
             	2) divide by 0 error'''
 
-        	if(gOperation == 1):
-            		print(gNum1, " + ", gNum2, " = ",
-                    	self.add(gNum1, gNum2))
-        	elif (gOperation == 2):
-            		print(gNum1, " - ", gNum2, " = ",
-                    	self.subtract(gNum1, gNum2))
-        	elif (gOperation == 3):
-            		print(gNum1, " x ", gNum2, " = ",
-                    	self.multiply (gNum1, gNum2))
-        	elif (gOperation == 4):
-            		if(gNum2 == 0):
-                		print('Divide by 0 error')
+        	if(self.gOperation == 1):
+            		print(self.gNum1, " + ", self.gNum2, " = ",
+                    	self.add(self.gNum1, self.gNum2))
+        	elif (self.gOperation == 2):
+            		print(self.gNum1, " - ", self.gNum2, " = ",
+                    	self.subtract(self.gNum1, self.gNum2))
+        	elif (self.gOperation == 3):
+            		print(self.gNum1, " x ", self.gNum2, " = ",
+                    	self.multiply (self.gNum1, self.gNum2))
+        	elif (self.gOperation == 4):
+            		if(self.gNum2 == 0):
+                		print('Error: Divide by 0')
             		else:
-                		print(gNum1, " / ", gNum2, " = ",
-                    		self.divide(gNum1, gNum2))
+                		print(self.gNum1, " / ", self.gNum2, " = ",
+                    		self.divide(self.gNum1, self.gNum2))
         	else:
             		self.gOperation = 0
-            		print ('error')
+            		print ('Error: Invalid Operator')
 
         	return
 
-#c1 = Calculator()
-#c1.main()
